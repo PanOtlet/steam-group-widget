3edaeec0bac74cfc1debe16e5ae4c28c8355b209
Pan Otlet <otlet0@gmail.com>
2015-03-03 23:32:34 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# steam-group-widget
-Show your group like a widget!
+=== Plugin Name ===
+Contributors: otlet
+Donate link: http://panotlet.tk
+Tags: steam, community, group, widget
+Requires at least: 3.0.1
+Tested up to: 4.1.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plugin that adds a widget with a group of steam
+
+== Description ==
+
+plugin that adds a widget with a group of Steam.
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add widget: Steam Group
+4. Complete the fields
+5. Save
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+`<?php code(); // goes in backticks ?>`
---
+++ steam-group-widget.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Plugin Name: Steam Community Group Widget
+Plugin URI: http://panotlet.tk/steam-group-widget/
+Description: Show your group like a widget!
+Version: 1.00
+Author: Paweł Otlewski
+Author URI: http://panotlet.tk/
+*/
+
+class otletsg_widget extends WP_Widget {
+
+	function __construct() {
+		parent::__construct(
+			'otletsg_widget',
+			__('Steam Group', 'otletsg_widget_domain'),
+			array( 'description' => __( 'Steam Group Widget to show a group from Steam', 'otletsg_widget_domain' ), ) 
+		);
+	}
+
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'Steam Group', 'otletsg_widget_domain' );
+		}
+		if ( isset( $instance[ 'adress' ] ) ) {
+			$adress = $instance[ 'adress' ];
+		}
+		else {
+			$adress = __( 'http://steamcommunity.com/groups/ts23games', 'otletsg_widget_domain' );
+		}
+	?>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'adress' ); ?>">Adress</label> 
+	<input class="widefat" id="<?php echo $this->get_field_id( 'adress' ); ?>" name="<?php echo $this->get_field_name( 'adress' ); ?>" type="text" value="<?php echo esc_attr( $adress ); ?>" />
+	</p>
+	<?php 
+	}
+
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+		$groupURL = apply_filters( 'widget_text', $instance['adress'] );
+		$xml = simplexml_load_file($groupURL . '/memberslistxml/');
+		echo '
+		<table>
+			<tr>
+				<td rowspan="5" width="30%"><img src="'.$xml->groupDetails->avatarFull.'" width="100%">   </td>
+				<td><strong>Group Name:</strong> '.$xml->groupDetails->groupName.'</td>
+			</tr>
+			<tr>
+				<td><strong>Member count:</strong> '.$xml->groupDetails->memberCount.'</td>
+			</tr>
+			<tr>
+				<td><strong>In Game:</strong> '.$xml->groupDetails->membersInGame.'</td>
+			</tr>
+			<tr>
+				<td><strong>Online:</strong> '.$xml->groupDetails->membersOnline.'</td>
+			</tr>
+			<tr>
+				<td><a href="' . $groupURL . '">Show this group</a></td>
+			</tr>
+		</table>';
+		echo $args['after_widget'];
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['adress'] = ( ! empty( $new_instance['adress'] ) ) ? strip_tags( $new_instance['adress'] ) : '';
+		return $instance;
+		}
+	}
+
+	function otletsg_load_widget() {
+		register_widget( 'otletsg_widget' );
+	}
+	add_action( 'widgets_init', 'otletsg_load_widget' );
